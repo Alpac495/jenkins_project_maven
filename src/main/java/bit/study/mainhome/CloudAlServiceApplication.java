9a31549b48a3501327ad6c697c135e01eb7f04cf
alpaca <dragonpepper95@gmail.com>
2023-05-12 12:31:28 +0900

    first

---
+++ src/main/java/bit/study/mainhome/CloudAlServiceApplication.java
@@ -0,0 +1,13 @@
+package bit.study.mainhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudAlServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudAlServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bit/study/mainhome/CloudAlServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package bit.study.mainhome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudAlServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
